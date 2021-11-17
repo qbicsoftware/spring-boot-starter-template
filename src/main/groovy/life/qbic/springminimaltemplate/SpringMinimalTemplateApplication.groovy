30d5dad0ccb67b02c71a6258391e79958b3dbe3f
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-11-17 15:29:19 +0100

    Update structure

---
+++ src/main/groovy/life/qbic/springminimaltemplate/SpringMinimalTemplateApplication.groovy
@@ -0,0 +1,13 @@
+package life.qbic.springminimaltemplate
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringMinimalTemplateApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringMinimalTemplateApplication, args)
+	}
+
+}
---
+++ src/test/groovy/life/qbic/springminimaltemplate/SpringMinimalTemplateApplicationTests.groovy
@@ -0,0 +1,13 @@
+package life.qbic.springminimaltemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMinimalTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
